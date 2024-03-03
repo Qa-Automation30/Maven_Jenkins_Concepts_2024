d8d0545b3b636ce94d2aaf2d2fb9895975f7be91
bitbucket_29 <vivek.srivastavacs65@gmail.com>
2024-03-03 20:32:11 +0530

    commit

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,49 @@
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class BaseTest {
+    FileInputStream fis;
+    Properties prop;
+    @BeforeSuite
+    void setup() throws IOException {
+        fis = new FileInputStream(System.getProperty("user.dir")+"\\src\\prop.config");
+        prop = new Properties();
+        prop.load(fis);
+        // First need to set up the env on which you want to run your test cases-->
+        // Second need to pass the URL for which you need to test, means QA URL, UAT Url, Dev URL
+        if(System.getProperty("env").equalsIgnoreCase("QA")){
+            setupBrowser();
+        }
+        else if(System.getProperty("env").equalsIgnoreCase("UAT")){
+            setupBrowser();
+        }
+        else {
+            setupBrowser();
+        }
+
+    }
+    void setupBrowser(){
+           if(System.getProperty("browser").equalsIgnoreCase("chrome")){
+                System.out.println("Chrome driver Launched with below URL");
+
+           }
+           else if(System.getProperty("browser").equalsIgnoreCase("firefox")){
+               System.out.println("firefox driver Launched with below URL");
+           }
+           else {
+               System.out.println("edge driver Launched with below URL");
+           }
+    }
+
+    @AfterSuite
+    void tearDown(){
+
+    }
+}
--- src/test/java/SanitySuite/SanityTwo.java
+++ src/test/java/SanitySuite/SanityTwo.java
@@ -6,7 +6,6 @@ import org.testng.annotations.Test;
 public class SanityTwo {
     @Test
     void sanityTwo_1(){
-        Assert.assertTrue(false);
         System.out.println("sanityTwo_1");
     }
     @Test
