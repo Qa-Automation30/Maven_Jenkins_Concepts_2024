450a1d7d636d90cb3cd30260b3daaf6f6593c5e1
bitbucket_29 <vivek.srivastavacs65@gmail.com>
2024-03-06 21:38:31 +0530

    commit

---
+++ src/test/java/concept_1_Jenkins/One.java
@@ -0,0 +1,23 @@
+package concept_1_Jenkins;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class One {
+    WebDriver driver;
+    @Test
+    void test() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.get("https://www.google.com/");
+        Thread.sleep(2000);
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+}
